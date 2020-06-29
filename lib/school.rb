d0453fafed9e0f699cf8d95d7bacaffd02ef9bb1
Derek Turner <turner.derek.b@gmail.com>
2020-06-29 15:56:07 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+class School
+
+
+    attr_accessor :roster
+
+    def initialize roster
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @name = name
+        @grade = grade
+        if roster.has_key?(grade)
+            roster[grade] << name
+        else 
+            roster[grade] = [name]
+        end
+    end
+
+    def grade (num)
+        roster[num]
+    end
+
+    def sort
+        roster.each do |grade, names_array|
+            roster[grade] = names_array.sort
+        end
+    end
+   
+end
