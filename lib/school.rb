f73811cdeabd33dfcf69a5e5064f9c442436819b
Gerald Bivens <geraldbivens@gmail.com>
2020-07-06 12:34:47 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+    attr_reader :school_name, :roster
+    
+    def initialize(school_name) 
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+        # keys are grade levels
+        # values are arrays of student names
+    end
+
+    # grade will be the keys in the roster hash
+    def grade(number)
+        # hash[new_key]
+        @roster[number] 
+    end
+
+    def add_student(student_name, grade)
+    end
+
+    def sort
+    end
+end
+ 
